158bb02cf300e6aa41b9320a30e86c74e427cc40
SriSabarish2021 <srisabarish003@gmail.com>
2025-05-09 21:55:08 +0530

    sd

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div className='appe'>
+    <h1 className='app__h1'>say hello</h1>
+    <h2 className='app__h2'>hello world</h2>
+   </div>
+  )
+}
+
+export default App
